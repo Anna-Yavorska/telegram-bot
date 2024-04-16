8e83c4421a22a7e789441704682efbc810c727f7
Anna Yavorska <aniayavorsk@gmail.com>
2024-04-16 09:03:31 +0200

    write some dialog

--- TelegramBot/src/main/java/ua/javarush/MyFirstTelegramBot.java
+++ TelegramBot/src/main/java/ua/javarush/MyFirstTelegramBot.java
@@ -6,6 +6,7 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 import static ua.javarush.TelegramBotUtils.*;
 
 public class MyFirstTelegramBot extends TelegramLongPollingBot {
@@ -22,10 +23,22 @@ public class MyFirstTelegramBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         Long chatId = getChatId(update);
-        String firstName = update.getMessage().getChat().getFirstName();
-        String welcomeText = String.format("Привіт, майбутній програміст %s!", firstName);
-        SendMessage message = createMessage(chatId, welcomeText);
-        sendApiMethodAsync(message);
+
+        if (update.hasMessage() && update.getMessage().getText().equals("/start")) {
+            String welcomeText = "Привіт, майбутній програміст!";
+            SendMessage message = createMessage(chatId, welcomeText);
+            sendApiMethodAsync(message);
+        }
+
+        if (update.hasMessage() && update.getMessage().getText().toLowerCase().contains("привіт")) {
+            SendMessage message = createMessage(chatId, "Привіт, як тебе звуть?");
+            sendApiMethodAsync(message);
+        }
+
+        if (update.hasMessage() && update.getMessage().getText().toLowerCase().contains("мене звуть")) {
+            SendMessage message = createMessage(chatId, "Радий знайомству, я — *Кіт*");
+            sendApiMethodAsync(message);
+        }
     }
 
     public static void main(String[] args) throws TelegramApiException {
